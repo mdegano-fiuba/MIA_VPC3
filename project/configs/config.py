cfc26c7ee442c39074f96f992fe28c7abbdfeb95
mdegano-fiuba <mdegano.ext@fi.uba.ar>
2025-12-05 21:34:06 -0300

    v0 config.py

---
+++ project/configs/config.py
@@ -0,0 +1,17 @@
+# configs/config.py
+
+import yaml
+import os
+
+# Obtener la ruta absoluta del archivo YAML relativo a este script
+CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config.yaml")
+
+# Verificar que el archivo exista
+if not os.path.exists(CONFIG_PATH):
+    raise FileNotFoundError(f"Falta el archivo de configuración: {CONFIG_PATH}")
+
+# Cargar el YAML
+with open(CONFIG_PATH, "r") as f:
+    config = yaml.safe_load(f)
+
+
