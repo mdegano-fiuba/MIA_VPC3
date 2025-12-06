e52067440dc18daad95eac396345efa8971297f9
mdegano-fiuba <mdegano.ext@fi.uba.ar>
2025-12-06 20:59:57 -0300

    v0 metrics

--- project/evaluation/metrics.py
+++ project/evaluation/metrics.py
@@ -1,10 +1,10 @@
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
 def compute_all_metrics(labels, preds):
-    return {
+    return {'eval_metrics': {
         "eval_accuracy": accuracy_score(labels, preds),
         "eval_f1": f1_score(labels, preds),
         "eval_precision": precision_score(labels, preds),
         "eval_recall": recall_score(labels, preds)
-    }
+    }}
 
