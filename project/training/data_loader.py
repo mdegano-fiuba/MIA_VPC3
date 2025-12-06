faaa2db781e709285b39d66fe64aa490df0ca379
mdegano-fiuba <mdegano.ext@fi.uba.ar>
2025-12-06 00:36:17 -0300

    v0 correcciones hugging face

--- project/training/data_loader.py
+++ project/training/data_loader.py
@@ -1,48 +1,60 @@
 import torch
 from torch.utils.data import DataLoader
 from datasets import load_dataset
-from sklearn.model_selection import train_test_split
 from training.augmentations import get_train_transforms, get_val_transforms
 from configs.config import config
 
 class CatsDogsDataset(torch.utils.data.Dataset):
-    def __init__(self, images, labels, transform=None):
-        self.images = images
-        self.labels = labels
+    """
+    Dataset personalizado para Cats vs Dogs.
+    Toma la imagen y la etiqueta desde Hugging Face dataset.
+    """
+    def __init__(self, ds, transform=None):
+        self.ds = ds
         self.transform = transform
 
     def __len__(self):
-        return len(self.images)
+        return len(self.ds)
 
     def __getitem__(self, idx):
-        img = self.images[idx]
-        label = self.labels[idx]
+        idx = int(idx)  # Convertir a int puro para PyTorch
+        img = self.ds[idx]['image']  # PIL Image
+        label = self.ds[idx]['labels']  
         if self.transform:
             img = self.transform(img)
         return img, label
 
 def get_dataloaders():
-    # Cargar dataset original
-    dataset = load_dataset(config['dataset']['name'])["train"]  # Hugging Face solo tiene train
-    images = dataset["image"]
-    labels = dataset["labels"]
+    """
+    Carga el dataset, realiza split balanceado y devuelve DataLoaders.
+    """
+    # 1) Cargar dataset completo
+    dataset = load_dataset(config['dataset']['name'])
 
-    # Split estratificado
-    train_imgs, val_imgs, train_labels, val_labels = train_test_split(
-        images,
-        labels,
+    # 2) Hacer split train/val balanceado
+    split = dataset['train'].train_test_split(
         test_size=config['dataset']['val_split'],
-        stratify=labels,
-        random_state=config['dataset']['seed']
+        stratify_by_column='labels',
+        seed=config['dataset']['seed']
     )
+    train_ds = CatsDogsDataset(split['train'], transform=get_train_transforms(config['dataset']['image_size']))
+    val_ds = CatsDogsDataset(split['test'], transform=get_val_transforms(config['dataset']['image_size']))
 
-    # Crear datasets PyTorch con augmentations
-    train_ds = CatsDogsDataset(train_imgs, train_labels, transform=get_train_transforms(config['dataset']['image_size']))
-    val_ds = CatsDogsDataset(val_imgs, val_labels, transform=get_val_transforms(config['dataset']['image_size']))
-
-    # DataLoaders
-    train_loader = DataLoader(train_ds, batch_size=config['dataset']['batch_size'], shuffle=True)
-    val_loader = DataLoader(val_ds, batch_size=config['dataset']['batch_size'], shuffle=False)
+    # 3) Crear DataLoaders
+    train_loader = DataLoader(
+        train_ds,
+        batch_size=config['dataset']['batch_size'],
+        shuffle=True,
+        num_workers=2,  
+        pin_memory=True
+    )
+    val_loader = DataLoader(
+        val_ds,
+        batch_size=config['dataset']['batch_size'],
+        shuffle=False,
+        num_workers=2,
+        pin_memory=True
+    )
 
     return train_loader, val_loader
 
