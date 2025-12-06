5569a69f9d0a150bcf1c9d336d8f01fbe65f649c
mdegano-fiuba <mdegano.ext@fi.uba.ar>
2025-12-05 23:21:15 -0300

    v0 correcciones de entrenamiento

--- project/training/model_builder.py
+++ project/training/model_builder.py
@@ -1,28 +1,25 @@
-# training/model_builder.py
 import torch
-from transformers import MobileViTForImageClassification
+from transformers import AutoImageProcessor, MobileViTForImageClassification
 from configs.config import config
 
 def get_model():
-    """
-    Carga MobileViT preentrenado desde Hugging Face,
-    adapta la cabeza a num_classes y deja solo la cabeza entrenable.
-    """
+    model_name = config['model']['name']
     num_classes = config['model']['num_classes']
-    model_name = config['model']['name']  
+    freeze_blocks = config['model']['frozen_blocks']
 
-    # Cargar MobileViT con la cabeza adaptada a num_classes
-    model = MobileViTForImageClassification.from_pretrained(
-        model_name,
-        num_labels=num_classes
-    )
+    # Cargar modelo preentrenado
+    model = MobileViTForImageClassification.from_pretrained(model_name)
 
-    # Congelar todo el backbone
+    # Reemplazar la cabeza final (classifier) por el número de clases de Cats vs Dogs
+    in_features = model.classifier.in_features
+    model.classifier = torch.nn.Linear(in_features, num_classes)
+
+    # Congelar bloques iniciales y embeddings
     for name, param in model.named_parameters():
-        if "classifier" in name:
-            param.requires_grad = True  # cabeza entrenable
-        else:
-            param.requires_grad = False  # backbone congelado
+        if any(name.startswith(f"mobilevit.encoder.{i}") for i in range(freeze_blocks)):
+            param.requires_grad = False
+        elif name.startswith("embeddings"):
+            param.requires_grad = False
 
-    return model
+    return model, AutoImageProcessor.from_pretrained(model_name)
 
